057aa51d61c6d1011c0f15997d1212e5cbee662e
AJ Ernandes <aj.ernandes01@gmail.com>
2021-07-01 09:25:47 -0400

    more functionality

--- main.py
+++ main.py
@@ -16,10 +16,9 @@ class Curtains(pygame.sprite.Sprite):
         self.image.blit(self.sofaImage, (0, 0))
         self.rect = self.image.get_rect()
         self.pos_y = pos[1]
-        self.speed_y = 0
+        self.speed_y = 2
 
     def update(self):
-        self.speed_y = 1
         self.pos_y += self.speed_y
         self.rect.center = (self.pos[0], self.pos_y)
         if self.pos_y > self.screen.get_height():
@@ -49,6 +48,10 @@ class Sofa(pygame.sprite.Sprite):
         self.sofaImage = pygame.image.load("sofa.png")
         self.image.blit(self.sofaImage, (0, 0))
     
+    def update(self):
+        self.pos_x = pygame.mouse.get_pos()[0]
+        self.rect.x = self.pos_x
+    
     def left(self):
         if(not self.pos_x < 0):
             self.pos_x -= 15
@@ -62,43 +65,73 @@ class Sofa(pygame.sprite.Sprite):
 
 def run_game():
     pygame.init()
+    pygame.font.init()
     screen = pygame.display.set_mode((400, 640))
+    pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
     pygame.display.set_caption("SofaShop")
     clock = pygame.time.Clock()
     counter = 0
+    sofaCounter = 0
     running = True
+    playing = True
     curtains = pygame.sprite.Group()
 
+   
     sofa = pygame.sprite.GroupSingle()
     
-    sofa.add(Sofa((200, 580), screen))
+    sofa.add(Sofa((200, 550), screen))
     while running:
         counter += 1
-        if (counter % 100) == 0:
-            curtains.add(Curtains((random.randint(20, 400), 0), screen))
-        curtains.update()
+        if (playing):
+            if (counter % 50) == 0:
+                curtains.add(Curtains((random.randint(20, 400), 0), screen))
+            curtains.update()
 
         for curtain in curtains:
-            collide = curtain.rect.colliderect(sofa.sprites()[0].rect)
+            try:
+                collide = curtain.rect.colliderect(sofa.sprites()[0].rect)
+            except (IndexError):
+                continue
             if collide:
                 if sofa.sprites()[0].color == curtain.color:
+                    sofaCounter += 1
+                    curtains.remove(curtain)
                     sofa.sprites()[0].newColor()
                 else:
-                    print("fail")
+                    playing = False
+                    curtains.empty()
+                    sofa.empty()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            key_input = pygame.key.get_pressed()  
-            if key_input[pygame.K_LEFT]:
-                sofa.sprites()[0].left()
-            if key_input[pygame.K_RIGHT]:
-                sofa.sprites()[0].right()
-        
+            try:
+                if event.type == pygame.MOUSEMOTION:
+                    sofa.sprites()[0].update()
+            except (IndexError):
+                continue
 
+        while not playing:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+
+            screen.fill((255, 0, 0))
+            
+            deadFont = pygame.font.SysFont("Arial", 20)
+            text = deadFont.render("You did not ♫ match your curtains too ♫", True, (0,0,0))
+
+            screen.blit(text, dest=(55,320))
+            pygame.display.flip()
+            clock.tick(60)
+        
         screen.fill((255, 255, 255))
         curtains.draw(screen)
         sofa.draw(screen)
+
+        counterFont = pygame.font.SysFont("Arial", 14)
+        text = counterFont.render("Curtains Matched: " + str(sofaCounter), True, (0,0,0))
+        screen.blit(text, dest=(380,10))
 
         pygame.display.flip()
         clock.tick(60)
