5f2f294d1bcfa6acbb4b8c14f900b86111410a48
AJ Ernandes <aj.ernandes01@gmail.com>
2021-07-03 09:51:08 -0400

    difficulty curve, higher level means faster falling

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Curtains(pygame.sprite.Sprite):
         self.screen = screen
         self.pos = pos
         self.color = random.choice(colors)
-        self.image = pygame.Surface((50, 50), pygame.SRCALPHA)
+        self.image = pygame.Surface((100 , 100), pygame.SRCALPHA)
         self.image.fill(self.color)
         self.sofaImage = pygame.image.load("curtains.png")
         self.image.blit(self.sofaImage, (0, 0))
@@ -32,7 +32,7 @@ class Sofa(pygame.sprite.Sprite):
         self.pos = pos
         self.screen = screen
         self.color = random.choice(colors)
-        self.image = pygame.Surface((50, 50), pygame.SRCALPHA)
+        self.image = pygame.Surface((100, 100), pygame.SRCALPHA)
         self.image.fill(self.color)
         self.sofaImage = pygame.image.load("sofa.png")
         self.image.blit(self.sofaImage, (0, 0))
@@ -43,7 +43,7 @@ class Sofa(pygame.sprite.Sprite):
 
     def newColor(self):
         self.color = random.choice(colors)
-        self.image = pygame.Surface((50, 50), pygame.SRCALPHA)
+        self.image = pygame.Surface((100, 100), pygame.SRCALPHA)
         self.image.fill(self.color)
         self.sofaImage = pygame.image.load("sofa.png")
         self.image.blit(self.sofaImage, (0, 0))
@@ -71,7 +71,8 @@ def run_game():
         pygame.mixer.music.play(-1)
     except:
         print("Issue with music, skipping...")
-    screen = pygame.display.set_mode((400, 640))
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    w, h = pygame.display.get_surface().get_size()
     pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
     pygame.display.set_caption("Sofa Shop - The Game")
     clock = pygame.time.Clock()
@@ -86,12 +87,13 @@ def run_game():
    
     sofa = pygame.sprite.GroupSingle()
     
-    sofa.add(Sofa((200, 550), screen))
+    sofa.add(Sofa((w/2, h*.8), screen))
     while running:
         counter += 1
         if (playing):
-            if (counter % 50 / level ** (1 / 3)) == 0:
-                curtains.add(Curtains((random.randint(20, 400), 0), screen))
+            if (counter % (80 - int(level ** (1 / 5) - 5.5))) == 0:
+                w, h = pygame.display.get_surface().get_size()
+                curtains.add(Curtains((random.randint(0, w), 0), screen))
             curtains.update()
 
         for curtain in curtains:
@@ -105,10 +107,11 @@ def run_game():
                     levelSofaCounter += 1
                     curtains.remove(curtain)
                     sofa.sprites()[0].newColor()
-                    if levelSofaCounter == level ** 2 + level + 6:
+                    if levelSofaCounter == level ** 1/2:
                         levelSofaCounter = 0
                         level += 1
                 else:
+                    #continue
                     playing = False
                     curtains.empty()
                     sofa.empty()
@@ -116,6 +119,10 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            #elif event.type == pygame.MOUSEBUTTONDOWN:
+            #    level +=1
+            #elif event.type == pygame.KEYDOWN:
+            #    level -=1
             try:
                 if event.type == pygame.MOUSEMOTION:
                     sofa.sprites()[0].update()
@@ -126,18 +133,22 @@ def run_game():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    return
 
             screen.fill((255, 0, 0))
             
-            deadFont = pygame.font.SysFont("Arial", 20)
-            deadFontsub = pygame.font.SysFont("Arial", 14)
+            deadFont = pygame.font.SysFont("Arial", 25)
+            deadFontsub = pygame.font.SysFont("Arial", 20)
             text = deadFont.render("You did not ♫ match your curtains too ♫", True, (0,0,0))
             subtext = deadFontsub.render("Final Score: " + str(sofaCounter), True, (0,0,0))
             finalLevel = deadFontsub.render("Final Level: " + str(level), True, (0,0,0))
+            endGame = deadFontsub.render("(Click anywhere to close)", True, (0,0,0))
 
-            screen.blit(text, dest=(55,320))
-            screen.blit(subtext, dest=(55,350))
-            screen.blit(finalLevel, dest=(55,380))
+            screen.blit(text, dest=(w*.4,320))
+            screen.blit(subtext, dest=(w*.4,350))
+            screen.blit(finalLevel, dest=(w*.4,370))
+            screen.blit(endGame, dest=(w*.4,390))
             pygame.display.flip()
             clock.tick(60)
         
@@ -148,11 +159,11 @@ def run_game():
         counterFont = pygame.font.SysFont("Arial", 14)
         text = counterFont.render("Curtains Matched: " + str(sofaCounter), True, (0,0,0))
         levelDisp = counterFont.render("Level: " + str(level), True, (0,0,0))
-        screen.blit(text, dest=(285,10))
-        screen.blit(levelDisp, dest=(285,25))
+        screen.blit(text, dest=(w*.9,10))
+        screen.blit(levelDisp, dest=(w*.9,25))
 
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(60 + level ** 2)
 
 
 run_game()
