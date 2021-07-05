2f83249bd04655b59f53e90604d9c8d27e7d5a0a
AJ Ernandes <aj.ernandes01@gmail.com>
2021-07-05 09:00:15 -0400

    added title screen

--- main.py
+++ main.py
@@ -3,6 +3,32 @@ import random
 
 colors = [(50,168,82), (173,247,19), (119,37,253), (117,182,240), (243,135,90)]
 
+class UnmadeLogo(pygame.sprite.Sprite):
+
+    def __init__(self, pos, screen):
+        super().__init__()
+        self.screen = screen
+        self.pos = pos
+        self.image = pygame.Surface((200 , 200), pygame.SRCALPHA)
+        self.logo = pygame.image.load("unmade.png")
+        self.image.blit(self.logo, (0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+
+class SofaLogo(pygame.sprite.Sprite):
+
+    def __init__(self, pos, screen):
+        super().__init__()
+        self.screen = screen
+        self.pos = pos
+        self.image = pygame.Surface((200 , 200), pygame.SRCALPHA)
+        self.logo = pygame.image.load("sofashop.png")
+        self.image.blit(self.logo, (0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+
 class Curtains(pygame.sprite.Sprite):
 
     def __init__(self, pos, screen):
@@ -62,19 +88,35 @@ class Sofa(pygame.sprite.Sprite):
         if(not self.pos_x > 400):
             self.pos_x += 15
             self.rect.x = self.pos_x
+def title_screen():
+    title = True
+    w, h = pygame.display.get_surface().get_size()
+    clock = pygame.time.Clock()
+    screen.fill((196,232,232))
+    titleFont = pygame.font.SysFont("Arial", 50)
+    levelDisp = titleFont.render("Sofa Shop - The Game", True, (0,0,0))
+    screen.blit(levelDisp, dest=(w*.39,h*.3))
+    titleFont = pygame.font.SysFont("Arial", 30)
+    levelDisp = titleFont.render("Press any key to begin...", True, (0,0,0))
+    screen.blit(levelDisp, dest=(w*.43,h*.6))
+    logos = pygame.sprite.Group()
+    logos.add(UnmadeLogo((w*.4,h*.5), screen))
+    logos.add(SofaLogo((w*.6, h*.5), screen))
+    logos.draw(screen)
+    pygame.display.flip()
+    while title:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            elif event.type == pygame.KEYDOWN:
+                title = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                title = False
+        clock.tick(60)
+    run_game()
 
 def run_game():
-    pygame.init()
-    pygame.font.init()
-    try:
-        pygame.mixer.music.load('8-bit.mp3')
-        pygame.mixer.music.play(-1)
-    except:
-        print("Issue with music, skipping...")
-    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     w, h = pygame.display.get_surface().get_size()
-    pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
-    pygame.display.set_caption("Sofa Shop - The Game")
     clock = pygame.time.Clock()
     counter = 0
     sofaCounter = 0
@@ -134,7 +176,7 @@ def run_game():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     return
 
-            screen.fill((255, 0, 0))
+            screen.fill((196,232,232))
             
             deadFont = pygame.font.SysFont("Arial", 25)
             deadFontsub = pygame.font.SysFont("Arial", 20)
@@ -163,6 +205,15 @@ def run_game():
         pygame.display.flip()
         clock.tick(60 + (sofaCounter + 4) ** 3)
 
-
-run_game()
+pygame.init()
+pygame.font.init()
+try:
+    pygame.mixer.music.load('8-bit.mp3')
+    pygame.mixer.music.play(-1)
+except:
+    print("Issue with music, skipping...")
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
+pygame.display.set_caption("Sofa Shop - The Game")
+title_screen()
 pygame.quit()
